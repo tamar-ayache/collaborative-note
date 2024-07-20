0cb6ff4bbb9335aba418d87f2dd47936f4d7d01d
Tamar Ayache <tamarayash12@gmail.com>
2024-07-20 13:55:58 +0300

    try

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// הגדרות Firebase שלך
+// הגדרות Firebase שלך :)
 const firebaseConfig = {
     apiKey: "AIzaSyAfMdP50KnBW-zKlbFMByC1IdfJRpWzw-E",
     authDomain: "tamar-7ed4a.firebaseapp.com",
