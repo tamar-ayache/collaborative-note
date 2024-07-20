56ee9602686c3a473ca11d1275d04b47360f2da5
Tamar Ayache <tamarayash12@gmail.com>
2024-07-20 22:05:34 +0300

    finish!!!!!

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import {initializeApp} from "firebase/app";
+import {getAuth} from "firebase/auth";
+import {getFirestore} from "firebase/firestore";
+import firebaseConfig from "./config.json";
+
+// הגדרות Firebase שלך :)
+
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export {auth, db};
